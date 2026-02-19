48807e6523baf9a97e6817ed6864cc9ad1ccbb18
Kriszti <molnarkrisztina400@gmail.com>
2026-02-19 08:04:50 +0100

    test commit

---
+++ bl.cs/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace bl.cs;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Csaba is very buta!");
+        Console.WriteLine("i will guess your number...");
+        Console.Write("enter a number: ");
+
+        int a = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine($"your number is {a}!");
+    }
+}
